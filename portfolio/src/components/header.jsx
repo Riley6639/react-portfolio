41b6ec6bc5dc875b5eb1f72b2bad95b677568edb
riley <mechamriley@gmail.com>
2024-10-30 21:55:26 -0600

    installed bootstrap and changed the main and app.jsx files

---
+++ portfolio/src/components/header.jsx
@@ -0,0 +1,9 @@
+// Import React library
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+//initialize react component
+
+
+// Create a functional component called Header
+export default const Header = () => {
